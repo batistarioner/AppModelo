abc9996384d91d2dcc2272ab5f8916b58d18cd70
rioner.batista <batista.rioner@gmail.com>
2021-11-02 13:03:02 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# AppModeloMVC
+# AppModeloMVC
+# AppModelo
---
+++ src/DevIO.UI.Site/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using DevIO.UI.Site.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevIO.UI.Site.Controllers
+{
+  public class HomeController : Controller
+  {
+    public IPedidoRepository _pedidoRepository;
+
+    public HomeController(IPedidoRepository pedidoRepository)
+    {
+      _pedidoRepository = pedidoRepository;
+    }
+
+    public IActionResult Index()
+    {
+      var pedido = _pedidoRepository.ObterPedido();
+
+      return View();
+    }
+  }
+}
